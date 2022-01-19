750d71c9043d6d51e7b622a9dfb300dc20c0ff0b
funny860 <phanindrarock@gmail.com>
2022-01-19 12:37:45 +0530

    commit to 'master'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'jquery';
+  name = 'Jquery Integration with Angular!';
+  isJqueryWorking: any;
+  ngOnInit() {
+    $(document).ready(function () {
+      // this.isJqueryWorking = 'Jquery is working !!!';
+      $('h3').hover(function () {
+        $('h3').css('color', 'slateblue');
+      });
+
+      $('button').click(function () {
+        $('#working').animate({ left: '250px' });
+      });
+    });
+  }
 }
